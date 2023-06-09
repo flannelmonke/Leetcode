75ca95b59996a86d9d4044c94cb7e72fd8490aa6
Khalil Naji <najiahm@sheridancollege.ca>
2023-06-09 01:22:58 -0400

    initial commit

---
+++ climbStairs.java
@@ -0,0 +1,15 @@
+public class climbStairs {
+    public int climbStairs(int n) {
+        int numberOfWays[] = new int[n+1];
+        for(int i =0; i <=n; i++){
+            if(i==0)
+            numberOfWays[i] = 1;
+            else if(i==1)
+            numberOfWays[i] = 1;
+            else{
+                numberOfWays[i] = numberOfWays[i-1] + numberOfWays[i-2];
+            }
+        }
+        return numberOfWays[n];
+    }
+}
